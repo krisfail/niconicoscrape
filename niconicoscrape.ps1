32c9c71367e3cc6c0998de27f9b19d6993928a2b
kris57 <49682577+yuuki76@users.noreply.github.com>
2021-02-28 12:07:31 +0900

    Init

---
+++ niconicoscrape.ps1
@@ -0,0 +1,3 @@
+For($i=1;$i -le 1000000;$i++){
+youtube-dl -u "ユーザー名に置き換えてください" -p "パスワードに置き換えてください"  -f best --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"  https://www.nicovideo.jp/watch/sm$i
+}
